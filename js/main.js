6cac1b3eba860601c735aa56aeab1c98f7dbd785
Dorideo <MikhailVarga2005@gmail.com>
2020-02-15 20:08:53 +0200

    Add slider

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.intro__sliderwrap').slick({
+      dots: true,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      arrows: false,
+    });
+  });
